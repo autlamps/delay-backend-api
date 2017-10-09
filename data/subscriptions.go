00a8bbf75f4d8a35abd95d1afb4a8b89f0de6dd7
Hayden Woodhead <hayden.sw@icloud.com>
2017-10-10 11:16:44 +1300

    Added Subscription models, service and New func with test

---
+++ data/subscriptions.go
@@ -0,0 +1,145 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var ErrNoNotificationMethods = errors.New("users - No notification methods specificed.")
+
+// Day is one of our three letter day codes
+type Day string
+
+// Defines our three letter day codes
+const (
+	MONDAY    Day = "Mon"
+	TUESDAY   Day = "Tue"
+	WEDNESDAY Day = "Wed"
+	THURSDAY  Day = "Thur"
+	FRIDAY    Day = "Fri"
+	SATURDAY  Day = "Sat"
+	SUNDAY    Day = "Sun"
+)
+
+// NewSubscription is received from called and transformed into a db backed Subscription
+type NewSubscription struct {
+	TripID          string
+	StopTimeID      string
+	Days            []Day
+	NotificationIDs []string
+	UserID          string
+}
+
+// Subscription contains all subscription info
+type Subscription struct {
+	ID              string
+	TripID          string
+	StopTimeID      string
+	UserID          string
+	Archived        bool
+	Created         time.Time
+	Monday          bool
+	Tuesday         bool
+	Wednesday       bool
+	Thursday        bool
+	Friday          bool
+	Saturday        bool
+	Sunday          bool
+	NotificationIDs []string
+}
+
+// SubscriptionStore defines methods for a concrete implementation
+type SubscriptionStore interface {
+	New(NewSubscription) (Subscription, error)
+}
+
+// SubscriptionService is our concrete psql implementation of the SubscriptionStore
+type SubscriptionService struct {
+	db *sql.DB
+}
+
+func InitSubscriptionService(db *sql.DB) *SubscriptionService {
+	return &SubscriptionService{db: db}
+}
+
+// New creates a new database backed Subscription, or returns an error
+func (ss *SubscriptionService) New(ns NewSubscription) (Subscription, error) {
+	// If no notification methods are specified then we send an error back
+	if len(ns.NotificationIDs) == 0 {
+		return Subscription{}, ErrNoNotificationMethods
+	}
+
+	id, err := uuid.NewRandom()
+
+	if err != nil {
+		return Subscription{}, fmt.Errorf("subscriptions - New: failed to generate uuid: %v", err)
+	}
+
+	s := Subscription{
+		ID:              id.String(),
+		TripID:          ns.TripID,
+		StopTimeID:      ns.StopTimeID,
+		Archived:        false,
+		Created:         time.Now().Round(time.Second),
+		NotificationIDs: ns.NotificationIDs,
+		UserID:          ns.UserID,
+	}
+
+	// Setup subscribed days, days not present will remain false as per golang default
+	for _, d := range ns.Days {
+		switch d {
+		case MONDAY:
+			s.Monday = true
+		case TUESDAY:
+			s.Tuesday = true
+		case WEDNESDAY:
+			s.Wednesday = true
+		case THURSDAY:
+			s.Thursday = true
+		case FRIDAY:
+			s.Friday = true
+		case SATURDAY:
+			s.Saturday = true
+		case SUNDAY:
+			s.Sunday = true
+		}
+	}
+
+	_, err = ss.db.Exec("INSERT INTO subscription (sub_id, trip_id, stoptime_id, user_id, archived, date_created, monday, tuesday, wednesday, thursday, friday, saturday, sunday) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+		s.ID,
+		s.TripID,
+		s.StopTimeID,
+		s.UserID,
+		s.Archived,
+		s.Created,
+		s.Monday,
+		s.Tuesday,
+		s.Wednesday,
+		s.Thursday,
+		s.Friday,
+		s.Saturday,
+		s.Sunday,
+	)
+
+	if err != nil {
+		return Subscription{}, fmt.Errorf("users - Subscription: Failed to insert subscription into db: %v", err)
+	}
+
+	for _, sn := range s.NotificationIDs {
+		_, err = ss.db.Exec("INSERT INTO sub_notification (sub_id, notification_id) VALUES ($1, $2)",
+			s.ID,
+			sn,
+		)
+
+		if err != nil {
+			return Subscription{}, fmt.Errorf("users - New: failed to link notification methods and subscription: %v", err)
+		}
+	}
+
+	return s, nil
+}
---
+++ data/subscriptions_test.go
@@ -0,0 +1,170 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func subSetup() (NotifyInfo, User, *sql.DB, error) {
+	db, err := sql.Open("postgres", dburl)
+
+	if err != nil {
+		return NotifyInfo{}, User{}, nil, fmt.Errorf("Failed to connect to db: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return NotifyInfo{}, User{}, nil, fmt.Errorf("Failed to ping db: %v", err)
+	}
+
+	us := InitUserService(db)
+
+	u, err := us.NewUser(NewUser{
+		"Bobby Tables",
+		"bobby.tables@example.com",
+		"correcthorsebatterystaple",
+	})
+
+	if err != nil {
+		return NotifyInfo{}, User{}, nil, fmt.Errorf("Failed to create new user: %v", err)
+	}
+
+	ns := InitNotifyInfoService(db)
+
+	n, err := ns.New(u.ID.String(), "p", "iPhone X", "1234456")
+
+	if err != nil {
+		return NotifyInfo{}, User{}, nil, fmt.Errorf("Failed to create new notify method: %v", err)
+	}
+
+	return n, u, db, nil
+}
+
+func TestSubscriptionService_New(t *testing.T) {
+	n, u, db, err := subSetup()
+
+	if err != nil {
+		t.Fatalf("Failed to setup: %v", err)
+	}
+
+	ss := InitSubscriptionService(db)
+
+	ns := NewSubscription{
+		TripID:          "df688c57-987c-4705-9e22-936342eb6e3f",
+		StopTimeID:      "5cce0bca-d489-43d7-b3cb-48e0df054c8a",
+		Days:            []Day{"Mon", "Tue", "Wed"},
+		NotificationIDs: []string{n.ID},
+		UserID:          u.ID.String(),
+	}
+
+	s, err := ss.New(ns)
+
+	if err != nil {
+		t.Fatalf("Failed to create subscription: %v", err)
+	}
+
+	row := db.QueryRow("SELECT sub_id, trip_id, stoptime_id, user_id, archived, date_created, monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM subscription WHERE sub_id = $1", s.ID)
+
+	dbs := Subscription{}
+
+	err = row.Scan(&dbs.ID, &dbs.TripID, &dbs.StopTimeID, &dbs.UserID, &dbs.Archived, &dbs.Created, &dbs.Monday, &dbs.Tuesday, &dbs.Wednesday, &dbs.Thursday, &dbs.Friday, &dbs.Saturday, &dbs.Sunday)
+
+	if err != nil {
+		t.Fatalf("Failed to read in sub from db: %v", err)
+	}
+
+	dbs.Created = dbs.Created.Local()
+
+	rows, err := db.Query("SELECT notification_id from sub_notification WHERE sub_id = $1", s.ID)
+
+	if err != nil {
+		t.Fatalf("Failed to read notification ids: %v", err)
+	}
+
+	for rows.Next() {
+		var id string
+
+		err := rows.Scan(&id)
+
+		if err != nil {
+			t.Fatalf("Failed to read in individual notification id: %v", err)
+		}
+
+		dbs.NotificationIDs = append(dbs.NotificationIDs, id)
+	}
+
+	if !reflect.DeepEqual(s, dbs) {
+		t.Fatalf("Sub not equal to db sub: Expected %v, got %v", s, dbs)
+	}
+
+	if dbs.Monday == false {
+		t.Fatalf("New Subscription differs from one returned from db: Expected %v, got %v", ns, dbs)
+	}
+
+	if dbs.Tuesday == false {
+		t.Fatalf("New Subscription differs from one returned from db: Expected %v, got %v", ns, dbs)
+	}
+
+	if dbs.Wednesday == false {
+		t.Fatalf("New Subscription differs from one returned from db: Expected %v, got %v", ns, dbs)
+	}
+
+	if ns.UserID != dbs.UserID {
+		t.Fatalf("New Subscription differs from one returned from db: Expected %v, got %v", ns, dbs)
+	}
+
+	if ns.TripID != dbs.TripID {
+		t.Fatalf("New Subscription differs from one returned from db: Expected %v, got %v", ns, dbs)
+	}
+
+	if ns.StopTimeID != dbs.StopTimeID {
+		t.Fatalf("New Subscription differs from one returned from db: Expected %v, got %v", ns, dbs)
+	}
+
+	if !reflect.DeepEqual(ns.NotificationIDs, dbs.NotificationIDs) {
+		t.Fatalf("New Subscription notification ids differs from one returned from db: Expected %v, got %v", ns.NotificationIDs, dbs.NotificationIDs)
+
+	}
+
+	err = subCleanUp(s, n, u, db)
+
+	if err != nil {
+		t.Fatalf("Failed to clean up")
+	}
+}
+
+func subCleanUp(s Subscription, ni NotifyInfo, u User, db *sql.DB) error {
+	_, err := db.Exec("DELETE FROM sub_notification WHERE sub_id = $1", s.ID)
+
+	if err != nil {
+		return fmt.Errorf("Failed to delete created notification: %v\n", err)
+	}
+
+	_, err = db.Exec("DELETE FROM subscription WHERE sub_id = $1", s.ID)
+
+	if err != nil {
+		return fmt.Errorf("Failed to delete created notification: %v\n", err)
+	}
+	_, err = db.Exec("DELETE FROM notification WHERE notification_id = $1", ni.ID)
+
+	if err != nil {
+		return fmt.Errorf("Failed to delete created notification: %v\n", err)
+	}
+	_, err = db.Exec("DELETE FROM tokens WHERE user_id = $1", u.ID)
+
+	if err != nil {
+		return fmt.Errorf("Failed to delete created user token: %v\n", err)
+	}
+
+	_, err = db.Exec("DELETE FROM users WHERE user_id = $1", u.ID)
+
+	if err != nil {
+		return fmt.Errorf("Failed to delete created user: %v\n", err)
+	}
+
+	db.Close()
+
+	return nil
+
+}
