6f2cb723aad466da3928c85b6b8464e3e582b8e1
Hayden Woodhead <hayden.sw@icloud.com>
2017-10-29 18:17:14 +1300

    Added calendar details to returned trip in /routes/:route_id/trips
    Needs proper testing for getCalendarForTrip and Calendar marshilling

--- static/trip.go
+++ static/trip.go
@@ -2,16 +2,31 @@ package static
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"time"
 )
 
 // Trip represents a trip as stored in the database
 type Trip struct {
-	ID        string `json:"id"`
-	RouteID   string `json:"route_id"`
-	ServiceID string `json:"service_id"`
-	GTFSID    string `json:"gtfsid"`
-	Headsign  string `json:"headsign"`
+	ID        string   `json:"id"`
+	RouteID   string   `json:"route_id"`
+	ServiceID string   `json:"service_id"`
+	GTFSID    string   `json:"gtfsid"`
+	Headsign  string   `json:"headsign"`
+	Calendar  Calendar `json:"calendar"`
+}
+
+type Calendar struct {
+	Start     time.Time `json:"-"`
+	End       time.Time `json:"-"`
+	Monday    bool      `json:"monday"`
+	Tuesday   bool      `json:"tuesday"`
+	Wednesday bool      `json:"wednesday"`
+	Thursday  bool      `json:"thursday"`
+	Friday    bool      `json:"friday"`
+	Saturday  bool      `json:"saturday"`
+	Sunday    bool      `json:"sunday"`
 }
 
 type Trips []Trip
@@ -64,10 +79,51 @@ func (ts *TripService) GetTripsByRouteID(id string) (Trips, error) {
 			return Trips{}, fmt.Errorf("trip - GetTripFromRouteID: Failed to scan: %v", err)
 		}
 
+		t.Calendar, err = ts.getCalendarForTrip(t)
+
+		if err != nil {
+			return Trips{}, fmt.Errorf("trip - GetTripsFromRouteID: failed to get cal: %v", err)
+		}
+
 		tps = append(tps, t)
 	}
 
 	return tps, nil
+}
+
+func (ts *TripService) getCalendarForTrip(t Trip) (Calendar, error) {
+	cal := Calendar{}
+
+	row := ts.db.QueryRow("SELECT monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM calendar WHERE service_id = $1", t.ServiceID)
+
+	err := row.Scan(&cal.Monday, &cal.Tuesday, &cal.Wednesday, &cal.Thursday, &cal.Friday, &cal.Saturday, &cal.Sunday)
+
+	if err != nil {
+		return Calendar{}, fmt.Errorf("trip - getCalendarForTrip: Failed to read in days from db: %v", err)
+	}
+
+	arrivalTimes := []time.Time{}
+
+	rows, err := ts.db.Query("SELECT arrival_time FROM stop_times WHERE trip_id = $1 ORDER BY stop_sequence ASC", t.ID)
+
+	for rows.Next() {
+		var arrival time.Time
+
+		err = rows.Scan(&arrival)
+
+		if err != nil {
+			return Calendar{}, fmt.Errorf("trip - getCalendarForTrip: Failed to read in times from db: %v", err)
+		}
+
+		arrival = arrival.Local()
+
+		arrivalTimes = append(arrivalTimes, arrival)
+	}
+
+	cal.Start = arrivalTimes[0]                 // First arrival time of the trip at a stop, the "start time"
+	cal.End = arrivalTimes[len(arrivalTimes)-1] // Last arrival of a trip at a stop the "end time"
+
+	return cal, nil
 }
 
 // IsEqual returns true if the given trip is equal to the trip the method is being called on
@@ -94,4 +150,20 @@ func (t Trip) IsEqual(x Trip) bool {
 	}
 
 	return true
+}
+
+func (c *Calendar) MarshalJSON() ([]byte, error) {
+	type Cal Calendar
+
+	js := struct {
+		*Cal
+		Start string `json:"start_time"`
+		End   string `json:"end"`
+	}{
+		Cal:   (*Cal)(c),
+		Start: c.Start.Format("15:04:05"),
+		End:   c.End.Format("15:04:05"),
+	}
+
+	return json.Marshal(js)
 }
